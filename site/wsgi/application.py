8bdc0c03bb387ddcf95d9a18d7240152872b137b
Руслан Корнев <oganer@gmail.com>
2012-03-05 14:17:59 +0400

    Добавил заголовок Policy, заработал кросдоменный json

--- site/wsgi/application.py
+++ site/wsgi/application.py
@@ -10,7 +10,7 @@ def make_json_app(import_name, **kwargs):
         response.status_code = (ex.code
                                 if isinstance(ex, HTTPException)
                                 else 500)
-        return response
+        return post_process_allow_origin(response)
 
     app = Flask(import_name, **kwargs)
 
@@ -22,12 +22,24 @@ def make_json_app(import_name, **kwargs):
 
 app = make_json_app(__name__)
 
+@app.route('/info/2210-MANNOL')
+def my_test():
+  return post_process_allow_origin(jsonify(itworks='yeah!'))
+
 @app.route('/')
 def hello_world():
   pyDict = {'one':1,'two':2}
-  response = jsonify(pyDict)
+  response = jsonify(one='1', two='2')
+  return post_process_allow_origin(response)
+
+def post_process_allow_origin(response):
+  #response = make_response(data)
+  #origin = request.headers.get('Origin', '')
+  #if origin.endswith('mydomain.com'):
+  response.headers['Access-Control-Allow-Origin'] = "*"
   return response
+  #response.headers['Access-Control-Allow-Credentials'] = 'true'
 
 if __name__ == '__main__':
   app.debug = True
-  app.run()
+  app.run('127.0.0.1', 5001)
