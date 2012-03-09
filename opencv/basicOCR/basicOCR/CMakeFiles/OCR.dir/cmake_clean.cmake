48641c1d5c23fcd6b05f1c0db1b15d58ff339b3f
Руслан Корнев <oganer@gmail.com>
2012-03-10 00:35:57 +0400

    basicOCR/ shirtDetection/ template-matching/

---
+++ opencv/basicOCR/basicOCR/CMakeFiles/OCR.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OCR.dir/main.o"
+  "CMakeFiles/OCR.dir/basicOCR.o"
+  "CMakeFiles/OCR.dir/preprocessing.o"
+  "OCR.pdb"
+  "OCR"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
