d26621c8e8f9f1e2ed6568ca139f529ce7d16213
Ruslan Kornev <oganer@gmail.com>
2012-03-13 14:21:41 +0400

    Объединил с functions.py

--- site/functions.py
+++ site/functions.py
@@ -1,4 +1,48 @@
+import pyscreenshot as ImageGrab
 import win32api, win32con
+import cv
+
+def find_match(file_name, template_array, roi, minimal, debug):
+
+  if not file_name:
+    file_name = 'img.png'
+    if roi:
+      im = ImageGrab.grab(roi)
+    else:
+      im = ImageGrab.grab()
+    im.save(file_name)
+
+  for id, template in enumerate(template_array):
+    img = cv.LoadImage(file_name, cv.CV_LOAD_IMAGE_COLOR)
+    tpl = cv.LoadImage(template, cv.CV_LOAD_IMAGE_COLOR)
+  
+    res = cv.CreateImage((img.width - tpl.width + 1, img.height - tpl.height + 1), cv.IPL_DEPTH_32F, 1)
+    cv.MatchTemplate(img, tpl, res, cv.CV_TM_SQDIFF)
+    (minval, maxval, minloc, maxloc) = cv.MinMaxLoc(res)
+
+    if debug:
+      print "minval:" + str(minval)
+      print "maxval:" + str(maxval)
+      print '"' + template + '": ' + str(minloc)
+      print ""
+
+      cv.Rectangle(img, 
+        (minloc[0], minloc[1]),
+        (minloc[0] + tpl.width, minloc[1] + tpl.height),
+      cv.Scalar(0, 1, 0, 0))
+      
+      cv.NamedWindow('image', cv.CV_WINDOW_AUTOSIZE)
+      cv.NamedWindow('template', cv.CV_WINDOW_AUTOSIZE)
+      cv.ShowImage('image', img)
+      cv.ShowImage('template', tpl)
+      
+      cv.WaitKey(0)
+
+      cv.DestroyWindow('image')
+      cv.DestroyWindow('template')
+    
+    if minval < minimal:
+      return [minloc[0], minloc[1]]
 
 def move(x, y):  
   win32api.SetCursorPos((x,y))
@@ -9,10 +53,10 @@ def click(x, y):
   win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
   win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-def handleRemoveReadonly(func, path, exc):
-  excvalue = exc[1]
-  if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
-      os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
-      func(path)
-  else:
-      raise
+#def handleRemoveReadonly(func, path, exc):
+#  excvalue = exc[1]
+#  if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+#      os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+#      func(path)
+#  else:
+#      raise
